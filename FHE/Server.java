321029c5eae037d4d9723c774cf1fdfe69fa37b2
parvthyk16 <parvthyk16@users.noreply.github.com>
2015-03-17 14:07:23 +0530

    Update Server.java

--- FHE/Server.java
+++ FHE/Server.java
@@ -1,48 +1,52 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
 public class Server {
-
-    private ServerSocket serverSocket;
-
-    public static void main(String[] args)
+  
+  public static void main(String[] args)
+  {
+        
+    try
     {
-        try
-        {
-            int port = 5001;
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket server = serverSocket.accept();
-            System.out.println("Server Started and listening to the port 1890");
-            int number[] = new int[25];
- 	    int i;
-           
-  	DataInputStream in =new DataInputStream(server.getInputStream());
-  	DataOutputStream out =new DataOutputStream(server.getOutputStream());
-	for(i=0;i<10;i++){
-          number[i]=in.readInt();
- 	  System.out.println(number[i]);
-        }		
+      String msg;
+      Socket s;
+      ServerSocket ser=new ServerSocket(4000);
+      System.out.println("SERVER LISTENING!!!\n");
+      s=ser.accept();
+      BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-        int c =number[0]+number[1];
-	int d= number[0]*number[1];
-	out.writeInt(c);
-	out.writeInt(d);
-
-server.close();          
-}
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+      OutputStream o=s.getOutputStream();
+      DataOutputStream out=new DataOutputStream(o);
     
-      
+      InputStream i=s.getInputStream();
+      DataInputStream in=new DataInputStream(i);
+         //System.out.println("ADDITION\n");
+        int c1,c2;
+        //msg=br.readLine();
+        c1=in.readInt();
+        c2=in.readInt();
+        System.out.println("c1="+c1);
+        System.out.println("c2="+c2);
+
+        System.out.println();
+        System.out.println("ADDITION\n");
+        int add=c1+c2;
+        System.out.println("c1+c2="+add);
+        System.out.println("\nMULTIPLICATION\n");
+        int mul=c1*c2;
+        System.out.println("c1*c2="+mul);
+ 
+        out.writeInt(add);
+        out.writeInt(mul);
+    
+
     }
+    
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+    
+    
+  }
+
 }
